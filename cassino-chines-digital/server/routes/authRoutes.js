4212c89d45d4cf40b29e1b9d24037cc424bec9cb
Bisteca777444 <almeidadiasjoseguilherme432@gmail.com>
2025-06-01 12:50:22 -0300

    Update authRoutes.js

--- cassino-chines-digital/server/routes/authRoutes.js
+++ cassino-chines-digital/server/routes/authRoutes.js
@@ -1,1 +1,11 @@
+import express from 'express';
+import { registerUser, loginUser, getUserBalance } from '../controllers/userController.js';
+
+const router = express.Router();
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/:id/balance', getUserBalance);
+
+export default router;
 // authRoutes.js - conteudo base
