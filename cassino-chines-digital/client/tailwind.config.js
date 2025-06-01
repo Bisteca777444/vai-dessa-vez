0191ffa3920c82df6d91dac4cc490279cf809638
Bisteca777444 <almeidadiasjoseguilherme432@gmail.com>
2025-06-01 12:52:50 -0300

    Update tailwind.config.js

--- cassino-chines-digital/client/tailwind.config.js
+++ cassino-chines-digital/client/tailwind.config.js
@@ -1,1 +1,20 @@
-// tailwind.config.js - conteudo base
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gold: '#FFD700',
+        redasiatic: '#8B0000',
+      },
+      fontFamily: {
+        asian: ['Noto Sans SC', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
